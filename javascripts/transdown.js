584fa21302ea5c100932c3884dd59354d65ef587
William Doane <Wil@drdoane.com>
2014-11-30 17:01:56 -0500

    Update transdown.js

--- javascripts/transdown.js
+++ javascripts/transdown.js
@@ -1,17 +1,17 @@
 var Transdown = {
 
-    updatePreviewAfterEachKeypress = function () {
+    updatePreviewAfterEachKeypress: function () {
         $('#text-to-transdownify').keyup(renderTranscriptPreview);
     },
     
-    renderTranscriptPreview = function () {
+    renderTranscriptPreview: function () {
         var textToTransdownify = document.getElementById('text-to-transdownify').value,
             outputText = transdownify(textToTransdownify);
         document.getElementById('live-preview').innerHTML = outputText;
         console.log("render");
     },
     
-    transdownify = function (text) {
+    transdownify: function (text) {
         return (text);
     }
 
